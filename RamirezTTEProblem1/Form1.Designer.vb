8d8822964b3fb035b8fb095488a3c66ddd957899
QuackDucker <Tarantetikstayo@gmail.com>
2025-12-12 10:02:08 +0800

    Add project files.

---
+++ RamirezTTEProblem1/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtTrackTitle = New System.Windows.Forms.TextBox()
+        Me.txtArtist = New System.Windows.Forms.TextBox()
+        Me.txtDuration = New System.Windows.Forms.TextBox()
+        Me.cmbGenre = New System.Windows.Forms.ComboBox()
+        Me.ButtonInsert = New System.Windows.Forms.Button()
+        Me.ButtonEdit = New System.Windows.Forms.Button()
+        Me.ButtonDelete = New System.Windows.Forms.Button()
+        Me.ButtonRead = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBoxHiddenID = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(34, 116)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Track Title:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(67, 162)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Artist:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(62, 196)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Genre:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(48, 239)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Duration:"
+        '
+        'txtTrackTitle
+        '
+        Me.txtTrackTitle.Location = New System.Drawing.Point(113, 113)
+        Me.txtTrackTitle.Name = "txtTrackTitle"
+        Me.txtTrackTitle.Size = New System.Drawing.Size(224, 20)
+        Me.txtTrackTitle.TabIndex = 4
+        '
+        'txtArtist
+        '
+        Me.txtArtist.Location = New System.Drawing.Point(113, 155)
+        Me.txtArtist.Name = "txtArtist"
+        Me.txtArtist.Size = New System.Drawing.Size(224, 20)
+        Me.txtArtist.TabIndex = 5
+        '
+        'txtDuration
+        '
+        Me.txtDuration.Location = New System.Drawing.Point(113, 232)
+        Me.txtDuration.Name = "txtDuration"
+        Me.txtDuration.Size = New System.Drawing.Size(224, 20)
+        Me.txtDuration.TabIndex = 7
+        '
+        'cmbGenre
+        '
+        Me.cmbGenre.FormattingEnabled = True
+        Me.cmbGenre.Items.AddRange(New Object() {"Pop", "Rock", "Jazz", "Hip-Hop", "Classical"})
+        Me.cmbGenre.Location = New System.Drawing.Point(108, 193)
+        Me.cmbGenre.Name = "cmbGenre"
+        Me.cmbGenre.Size = New System.Drawing.Size(229, 21)
+        Me.cmbGenre.TabIndex = 8
+        '
+        'ButtonInsert
+        '
+        Me.ButtonInsert.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonInsert.Location = New System.Drawing.Point(49, 275)
+        Me.ButtonInsert.Name = "ButtonInsert"
+        Me.ButtonInsert.Size = New System.Drawing.Size(169, 42)
+        Me.ButtonInsert.TabIndex = 9
+        Me.ButtonInsert.Text = "Add Track"
+        Me.ButtonInsert.UseVisualStyleBackColor = True
+        '
+        'ButtonEdit
+        '
+        Me.ButtonEdit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonEdit.Location = New System.Drawing.Point(264, 323)
+        Me.ButtonEdit.Name = "ButtonEdit"
+        Me.ButtonEdit.Size = New System.Drawing.Size(169, 42)
+        Me.ButtonEdit.TabIndex = 10
+        Me.ButtonEdit.Text = "Update Track"
+        Me.ButtonEdit.UseVisualStyleBackColor = True
+        '
+        'ButtonDelete
+        '
+        Me.ButtonDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonDelete.Location = New System.Drawing.Point(49, 323)
+        Me.ButtonDelete.Name = "ButtonDelete"
+        Me.ButtonDelete.Size = New System.Drawing.Size(169, 42)
+        Me.ButtonDelete.TabIndex = 11
+        Me.ButtonDelete.Text = "Delete Track"
+        Me.ButtonDelete.UseVisualStyleBackColor = True
+        '
+        'ButtonRead
+        '
+        Me.ButtonRead.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonRead.Location = New System.Drawing.Point(264, 275)
+        Me.ButtonRead.Name = "ButtonRead"
+        Me.ButtonRead.Size = New System.Drawing.Size(169, 42)
+        Me.ButtonRead.TabIndex = 12
+        Me.ButtonRead.Text = "Load Track"
+        Me.ButtonRead.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(391, 116)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(397, 126)
+        Me.DataGridView1.TabIndex = 13
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Perpetua Titling MT", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(144, 24)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(416, 34)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Music Studio Manager"
+        '
+        'TextBoxHiddenID
+        '
+        Me.TextBoxHiddenID.Location = New System.Drawing.Point(41, 418)
+        Me.TextBoxHiddenID.Name = "TextBoxHiddenID"
+        Me.TextBoxHiddenID.Size = New System.Drawing.Size(150, 20)
+        Me.TextBoxHiddenID.TabIndex = 16
+        Me.TextBoxHiddenID.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBoxHiddenID)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ButtonRead)
+        Me.Controls.Add(Me.ButtonDelete)
+        Me.Controls.Add(Me.ButtonEdit)
+        Me.Controls.Add(Me.ButtonInsert)
+        Me.Controls.Add(Me.cmbGenre)
+        Me.Controls.Add(Me.txtDuration)
+        Me.Controls.Add(Me.txtArtist)
+        Me.Controls.Add(Me.txtTrackTitle)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtTrackTitle As TextBox
+    Friend WithEvents txtArtist As TextBox
+    Friend WithEvents txtDuration As TextBox
+    Friend WithEvents cmbGenre As ComboBox
+    Friend WithEvents ButtonInsert As Button
+    Friend WithEvents ButtonEdit As Button
+    Friend WithEvents ButtonDelete As Button
+    Friend WithEvents ButtonRead As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBoxHiddenID As TextBox
+End Class
---
+++ RamirezTTEProblem1/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+
+    Private Sub ButtonInsert_Click(sender As Object, e As EventArgs) Handles ButtonInsert.Click
+        Dim query As String = "INSERT INTO tracks_tbl (Title, Artist, Duration, Genre) VALUES (@Title, @Artist, @Duration ,@Genre)"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=musicstudio_db;")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@Title", txtTrackTitle.Text)
+                    cmd.Parameters.AddWithValue("@Artist", (txtArtist.Text))
+                    cmd.Parameters.AddWithValue("@Duration", CInt(txtDuration.Text))
+                    cmd.Parameters.AddWithValue("@Genre", cmbGenre.Text)
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record insert successfully!")
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonRead_Click(sender As Object, e As EventArgs) Handles ButtonRead.Click
+        Dim query As String = "SELECT * FROM musicstudio_db.tracks_tbl;"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=musicstudio_db;")
+                Dim adapter As New MySqlDataAdapter(query, conn)
+                Dim table As New DataTable()
+                adapter.Fill(table)
+                DataGridView1.DataSource = table
+                DataGridView1.Columns("id").Visible = False
+                DataGridView1.Columns("is_deleted").Visible = False
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonEdit_Click(sender As Object, e As EventArgs) Handles ButtonEdit.Click
+        Dim query As String = "UPDATE `musicstudio_db`.`tracks_tbl` SET Title = @Title, Artist = @Artist, Duration = @Duration , Genre = @Genre WHERE `id`=id;"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=musicstudio_db;")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@Title", txtTrackTitle.Text)
+                    cmd.Parameters.AddWithValue("@Artist", (txtArtist.Text))
+                    cmd.Parameters.AddWithValue("@Duration", CInt(txtDuration.Text))
+                    cmd.Parameters.AddWithValue("@id", CInt(TextBoxHiddenID.Text))
+                    cmd.Parameters.AddWithValue("@Genre", cmbGenre.Text)
+
+                    Dim rowsAffected As Integer = cmd.ExecuteNonQuery()
+                    If rowsAffected > 0 Then
+                        MessageBox.Show("Record updated successfully!")
+                    Else
+                        MessageBox.Show("No record found with that ID.")
+                    End If
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonDelete_Click(sender As Object, e As EventArgs) Handles ButtonDelete.Click
+        Dim query As String = "UPDATE `musicstudio_db`.`tracks_tbl` SET `is_deleted`=1 WHERE `id`=id;"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=musicstudio_db;")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("Id", CInt(TextBoxHiddenID.Text))
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record deleted successfully!")
+                    txtTrackTitle.Clear()
+                    txtArtist.Clear()
+                    txtDuration.Clear()
+                    cmbGenre.ResetText()
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellDoubleClick
+        If e.RowIndex >= 0 Then
+            Dim selectedRow As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+            txtTrackTitle.Text = selectedRow.Cells("Title").Value.ToString()
+            txtArtist.Text = selectedRow.Cells("Artist").Value.ToString()
+            txtDuration.Text = selectedRow.Cells("Duration").Value.ToString()
+            cmbGenre.Text = selectedRow.Cells("Genre").Value.ToString()
+
+            TextBoxHiddenID.Text = selectedRow.Cells("id").Value.ToString()
+        End If
+    End Sub
+End Class
